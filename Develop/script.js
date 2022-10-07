f6a84585fc6d43674b368319968e5d8c0ec164fc
Sebastian <smcasanova17@hotmail.com>
2022-10-07 10:38:49 -0400

    reverted code back to when the prompt works with button

--- Develop/script.js
+++ Develop/script.js
@@ -6,19 +6,22 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-//---------------------------------------------------------------------------------------------
+//
 
 // Add event listener to generate button
 document.getElementById("generate").addEventListener("click",userInput)
-function userSelection(){
-  const getPassLength = ("please select password length"))
-  
-}
 function userInput(){
-  document.getElementById("generate").innerHTML userSelection()
+  document.getElementById("generate").innerHTML
+  var getPassLength = window.prompt("please select password length")
+  var getUpperCase = window.prompt("do you want your password to have uppercase characters?")
+  var getLowerCase = window.prompt("do you want your password to have lowercase character?")
+  var getnumeric = window.prompt("do you want your password to have numbers?")
+  var getSpecChar = window.prompt("do you want your passwaord to have special characters?")
 }
-// storing the prompt values here untill i figure out how to return the users selection
-  //var getUpperCase = window.prompt("do you want your password to have uppercase characters?")
-  //var getLowerCase = window.prompt("do you want your password to have lowercase character?")
-  //var getnumeric = window.prompt("do you want your password to have numbers?")
-  //var getSpecChar = window.prompt("do you want your passwaord to have special characters?")
+// ask user for length/ this should include a randomizer that will
+
+// be from 8 too 128 characters
+
+// ask user to confirm said char types and create randomizer
+
+// password is generated
