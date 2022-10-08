0455e0afed544da920c0e23ee9e5f9c741ef79fc
Sebastian <smcasanova17@hotmail.com>
2022-10-08 11:22:08 -0400

    corrected function declaration, understood how root code works, created correct question prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+
+  var getPassLength = window.prompt("please select password length")
+
+  var passwordLength = parseInt(getPassLength)
+
+  if(passwordLength > 128 || passwordLength < 8){
+    window.alert("please choose a number between 8 and 128")
+    return
+  }
+  
+  if(isNaN(passwordLength)){
+    window.alert("please choose a number value")
+    return
+  }
+
+
+  var getUpperCase = window.confirm("do you want your password to have uppercase characters?")
+  var getLowerCase = window.confirm("do you want your password to have lowercase character?")
+  var getnumeric = window.confirm("do you want your password to have numbers?")
+  var getSpecChar = window.confirm("do you want your passwaord to have special characters?")
+
+
+
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
+
 }
-//
+
 
 // Add event listener to generate button
-document.getElementById("generate").addEventListener("click",userInput)
-function userInput(){
-  document.getElementById("generate").innerHTML
-  var getPassLength = window.prompt("please select password length")
-  var getUpperCase = window.prompt("do you want your password to have uppercase characters?")
-  var getLowerCase = window.prompt("do you want your password to have lowercase character?")
-  var getnumeric = window.prompt("do you want your password to have numbers?")
-  var getSpecChar = window.prompt("do you want your passwaord to have special characters?")
-}
-// ask user for length/ this should include a randomizer that will
+generateBtn.addEventListener("click",writePassword);
 
-// be from 8 too 128 characters
 
-// ask user to confirm said char types and create randomizer
 
-// password is generated
+
+
+
+
+
+
+
+
